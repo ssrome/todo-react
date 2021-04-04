e05c68bbcfd7f99b779b15571e11402b60830b9e
Sabrina <ssamuelrome@gmail.com>
2021-04-04 01:42:27 +0100

    Added Bootstrap and ability to add item

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [addedItem, setAddedItem] = useState(null);
+  const [items, setItems] = useState([
+    {
+      itemName: "Pay bill",
+      complete: false,
+    },
+    {
+      itemName: "Clean car",
+      complete: false,
+    },
+  ]);
+
+  function updateAddedItem(event) {
+    setAddedItem(event.target.value);
+  }
+
+  function addItems(event) {
+    event.preventDefault();
+    setItems([
+      ...items,
+      {
+        itemName: addedItem,
+        complete: false,
+      },
+    ]);
+    console.log(items);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col>
+            <h1>TO DO LIST</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form onSubmit={addItems}>
+              <Col>
+                <Form.Group>
+                  <Form.Control
+                    type="input"
+                    placeholder="Add item"
+                    onChange={updateAddedItem}
+                  />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Button variant="primary" type="submit">
+                  Add
+                </Button>
+              </Col>
+            </Form>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ul>
+              {items.map(function (item, index) {
+                return <li key={index}>{item.itemName}</li>;
+              })}
+            </ul>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
-
-export default App;
