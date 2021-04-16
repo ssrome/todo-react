9fac7602e86aaed393ced8c1f6fc23564268d150
Sabrina <ssamuelrome@gmail.com>
2021-04-16 12:16:46 +0100

    Added buttons to list items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import ListGroup from "react-bootstrap/ListGroup";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -11,10 +13,12 @@ export default function App() {
   const [addedItem, setAddedItem] = useState(null);
   const [items, setItems] = useState([
     {
+      id: 1,
       itemName: "Pay bill",
       complete: false,
     },
     {
+      id: 2,
       itemName: "Clean car",
       complete: false,
     },
@@ -29,18 +33,26 @@ export default function App() {
     setItems([
       ...items,
       {
+        id: uuidv4(),
         itemName: addedItem,
         complete: false,
       },
     ]);
     console.log(items);
   }
+
+  function deleteItem(event) {
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
       <Container>
         <Row>
           <Col>
-            <h1>TO DO LIST</h1>
+            <header>
+              <h1>TO DO LIST</h1>
+            </header>
           </Col>
         </Row>
         <Row>
@@ -50,6 +62,7 @@ export default function App() {
                 <Form.Group>
                   <Form.Control
                     type="input"
+                    aria-label="add item input"
                     placeholder="Add item"
                     onChange={updateAddedItem}
                   />
@@ -65,11 +78,20 @@ export default function App() {
         </Row>
         <Row>
           <Col>
-            <ul>
+            <ListGroup>
               {items.map(function (item, index) {
-                return <li key={index}>{item.itemName}</li>;
+                return (
+                  <ListGroup.Item key={index}>
+                    {item.itemName}: {item.id}
+                    <span className="listButtons">
+                      <Button variant="success">Done</Button>{" "}
+                      <Button variant="light">Edit</Button>{" "}
+                      <Button variant="danger">Delete</Button>
+                    </span>
+                  </ListGroup.Item>
+                );
               })}
-            </ul>
+            </ListGroup>
           </Col>
         </Row>
       </Container>
