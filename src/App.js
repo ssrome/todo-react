edf8129bc45aba35e44d75333dc6d1ab9ae1ae7f
Sabrina <ssamuelrome@gmail.com>
2021-04-18 01:00:49 +0100

    Updated css

--- src/App.js
+++ src/App.js
@@ -1,133 +1,17 @@
-import React, { useState } from "react";
-import ToDoItem from "./ToDoItem";
+import React from "react";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Form from "react-bootstrap/Form";
-import ListGroup from "react-bootstrap/ListGroup";
-import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import ToDo from "./ToDo";
 
 export default function App() {
-  const [addedItem, setAddedItem] = useState(null);
-  //const [completedItems, setCompletedItems] = useState(null);
-  const [items, setItems] = useState([
-    {
-      itemName: "Pay bill",
-      complete: false,
-    },
-    {
-      itemName: "Clean car",
-      complete: false,
-    },
-  ]);
-
-  function updateAddedItem(event) {
-    setAddedItem(event.target.value);
-  }
-
-  function addItems(event) {
-    event.preventDefault();
-    setItems([
-      ...items,
-      {
-        itemName: addedItem,
-        complete: false,
-      },
-    ]);
-    console.log(items);
-  }
-
-  function deleteItem(index) {
-    const newItems = [...items];
-    newItems.splice(index, 1);
-    setItems(newItems);
-    console.log(items);
-  }
-
-  function completeItem(index) {
-    const newItems = [...items];
-    if (newItems[index].complete === false) {
-      newItems[index].complete = true;
-      setItems(newItems);
-      console.log(items);
-    } else if (newItems[index].complete === true) {
-      newItems[index].complete = false;
-      setItems(newItems);
-      console.log(items);
-    }
-  }
-
   return (
     <div className="App">
       <Container>
-        <Row>
-          <Col>
-            <header>
-              <h1>TO DO LIST</h1>
-            </header>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Form onSubmit={addItems}>
-              <Row>
-                <Col>
-                  <Form.Control
-                    type="text"
-                    aria-label="add item input"
-                    placeholder="Add item"
-                    autoComplete="off"
-                    onChange={updateAddedItem}
-                  />
-                </Col>
-                <Col xs lg={1}>
-                  {" "}
-                  <Button variant="primary" type="submit">
-                    Add
-                  </Button>
-                </Col>
-              </Row>
-            </Form>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <ListGroup>
-              {items.map(function (item, index) {
-                return (
-                  <ListGroup.Item key={index}>
-                    <Row>
-                      <Col>
-                        <ToDoItem
-                          item={item.itemName}
-                          complete={item.complete}
-                          index={index}
-                        />
-                      </Col>
-                      <Col xs lg={4} className="listButtons">
-                        <Button
-                          variant="success"
-                          onClick={() => completeItem(index)}
-                        >
-                          Complete
-                        </Button>{" "}
-                        <Button variant="light">Edit</Button>{" "}
-                        <Button
-                          variant="danger"
-                          onClick={() => deleteItem(index)}
-                        >
-                          Delete
-                        </Button>
-                      </Col>
-                    </Row>
-                  </ListGroup.Item>
-                );
-              })}
-            </ListGroup>
-          </Col>
-        </Row>
+        <header>
+          <h1>TO DO LIST</h1>
+        </header>
+        <ToDo />
       </Container>
     </div>
   );
---
+++ src/ToDo.js
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button";
+export default function ToDo() {
+  const [addedItem, setAddedItem] = useState(null);
+  const [items, setItems] = useState([
+    {
+      itemName: "Pay bill",
+      complete: false,
+    },
+    {
+      itemName: "Clean car",
+      complete: false,
+    },
+  ]);
+
+  function updateAddedItem(event) {
+    if (event.target.value.length > 0) {
+      setAddedItem(event.target.value);
+    } else {
+      return null;
+    }
+  }
+
+  function addItems(event) {
+    event.preventDefault();
+    if (addedItem && addedItem.length > 0) {
+      setItems([
+        ...items,
+        {
+          itemName: addedItem,
+          complete: false,
+        },
+      ]);
+    } else {
+      console.log(items);
+    }
+    console.log(items);
+  }
+
+  function deleteItem(index) {
+    const newItems = [...items];
+    newItems.splice(index, 1);
+    setItems(newItems);
+    console.log(items);
+  }
+
+  function completeItem(index) {
+    const newItems = [...items];
+    if (newItems[index].complete === false) {
+      newItems[index].complete = true;
+      setItems(newItems);
+      console.log(items);
+    } else if (newItems[index].complete === true) {
+      newItems[index].complete = false;
+      setItems(newItems);
+      console.log(items);
+    }
+  }
+
+  function showCompleteButton(complete) {
+    if (complete === false) {
+      return "Mark Complete";
+    } else if (complete === true) {
+      return "Undo Complete";
+    }
+  }
+  return (
+    <div>
+      <Row>
+        <Col>
+          <Form onSubmit={addItems}>
+            <Row>
+              <Col>
+                <Form.Control
+                  type="text"
+                  aria-label="add item input"
+                  placeholder="Add item"
+                  autoComplete="off"
+                  onChange={updateAddedItem}
+                />
+              </Col>
+              <Col xs lg={1}>
+                {" "}
+                <Button variant="primary" type="submit">
+                  Add
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <ListGroup>
+            {items.map(function (item, index) {
+              return (
+                <ListGroup.Item key={index}>
+                  <Row>
+                    <Col>
+                      <ToDoItem
+                        item={item.itemName}
+                        complete={item.complete}
+                        index={index}
+                      />
+                    </Col>
+                    <Col xs lg={4} className="listButtons">
+                      <Button
+                        variant="success"
+                        onClick={() => completeItem(index)}
+                      >
+                        {showCompleteButton(item.complete)}
+                      </Button>{" "}
+                      <Button variant="light">Edit</Button>{" "}
+                      <Button
+                        variant="danger"
+                        onClick={() => deleteItem(index)}
+                      >
+                        Delete
+                      </Button>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              );
+            })}
+          </ListGroup>
+        </Col>
+      </Row>
+    </div>
+  );
+}
